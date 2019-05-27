3fe81f22b83ba57463e061fa8fce46cafac77306
Jamie Lee <jlee@Jamies-MacBook-Pro.local>
2019-05-27 16:07:51 -0400

    adding files

---
+++ app/src/main/java/com/example/readyourresults/Help/HelpActivity.java
@@ -0,0 +1,15 @@
+package com.example.readyourresults.Help;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.readyourresults.R;
+
+public class HelpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_help);
+    }
+}
