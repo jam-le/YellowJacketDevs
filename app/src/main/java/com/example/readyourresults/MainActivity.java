ba547fcc2396871f9bfa2855cea6a0b6ad01d873
Jamie Lee <jlee@lawn-143-215-125-102.lawn.gatech.edu>
2019-05-29 13:27:19 -0400

    Added Help screen question buttons. Removed default overflow menu.

--- app/src/main/java/com/example/readyourresults/MainActivity.java
+++ app/src/main/java/com/example/readyourresults/MainActivity.java
@@ -56,12 +56,6 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
