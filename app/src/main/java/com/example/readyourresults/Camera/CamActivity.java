6c8692eb470d18c2e33ddcf0e3ac2a5ac0124eb1
vab10266 <vaudalexburton@gmail.com>
2019-05-29 12:34:02 -0400

    new camera branch

---
+++ app/src/main/java/com/example/readyourresults/Camera/CamActivity.java
@@ -0,0 +1,6 @@
+package com.example.readyourresults.Camera;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class CamActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/readyourresults/Camera/CamFragment.java
@@ -0,0 +1,6 @@
+package com.example.readyourresults.Camera;
+
+import android.support.v4.app.Fragment;
+
+public class CamFragment extends Fragment {
+}
