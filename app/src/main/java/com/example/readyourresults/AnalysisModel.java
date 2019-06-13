583ae0af7cee0bfb2c932319d879ac5346e5af36
vab10266 <vaudalexburton@gmail.com>
2019-06-13 11:25:19 -0400

    Began

--- app/src/main/java/com/example/readyourresults/AnalysisModel.java
+++ app/src/main/java/com/example/readyourresults/AnalysisModel.java
@@ -1,4 +1,17 @@
 package com.example.readyourresults;
 
+import com.google.firebase.ml.common.modeldownload.FirebaseLocalModel;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
 public class AnalysisModel {
+    public static void main(String[] args) {
+
+        FirebaseLocalModel localModel = new FirebaseLocalModel.Builder("app.assets.model")
+                .setAssetFilePath("manifest.json")
+                .build();
+        FirebaseModelManager.getInstance().registerLocalModel(localModel);
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+    }
 }
