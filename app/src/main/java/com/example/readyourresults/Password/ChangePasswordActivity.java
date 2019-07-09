dc272dd276fcf4c3e92a5d66249e8efcc5f0cd02
Matthew <mattjinks@gmail.com>
2019-07-09 02:03:54 -0400

    Implement Change Password:
    * Implement the Change Password functionilty under the Settings screen
    * User should be required to enter current password before being able to
    change their password

--- app/src/main/java/com/example/readyourresults/Password/ChangePasswordActivity.java
+++ app/src/main/java/com/example/readyourresults/Password/ChangePasswordActivity.java
@@ -2,37 +2,46 @@ package com.example.readyourresults.Password;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import com.google.android.material.textfield.TextInputLayout;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.readyourresults.MainActivity;
 import com.example.readyourresults.R;
+import com.google.android.material.textfield.TextInputLayout;
 
-public class CreatePasswordActivity extends AppCompatActivity {
+public class ChangePasswordActivity extends AppCompatActivity {
 
-    TextInputLayout editNewPassword, editRepeatPassword;
+    TextInputLayout editNewPassword, editRepeatPassword, editCurrentPassword;
     Button confirmButton, cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_password);
+        setContentView(R.layout.activity_change_password);
 
-        editNewPassword = (TextInputLayout) findViewById(R.id.new_password);
-        editRepeatPassword = (TextInputLayout) findViewById(R.id.repeat_password);
-        confirmButton = (Button) findViewById(R.id.confirm_button);
-        cancelButton = (Button) findViewById(R.id.cancel_button);
+        editCurrentPassword = (TextInputLayout) findViewById(R.id.current_pass_cp);
+        editNewPassword = (TextInputLayout) findViewById(R.id.new_password_cp);
+        editRepeatPassword = (TextInputLayout) findViewById(R.id.repeat_password_cp);
+        confirmButton = (Button) findViewById(R.id.confirm_button_cp);
+        cancelButton = (Button) findViewById(R.id.cancel_button_cp);
 
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String currentPassword = editCurrentPassword.getEditText().getText().toString().trim();
                 String newPassword = editNewPassword.getEditText().getText().toString().trim();
-
                 String repeatPassword = editRepeatPassword.getEditText().getText().toString().trim();
+
+                if (currentPassword.equals("")) {
+                    editCurrentPassword.setError("Field Can't be empty");
+                }
+
+                if (!getCurrentPassword().equals(currentPassword)) {
+                    //Current Password is incorrect
+                    editCurrentPassword.setError("Password Incorrect");
+                }
 
                 if (newPassword.equals("")) {
                     //there is no password
@@ -43,13 +52,15 @@ public class CreatePasswordActivity extends AppCompatActivity {
                     editRepeatPassword.setError("Field Can't be empty");
                 } else {
                     //save password
-                    if(newPassword.equals(repeatPassword)) {
+                    if(newPassword.equals(repeatPassword)
+                            && getCurrentPassword().equals(currentPassword)) {
+
                         SharedPreferences settings = getSharedPreferences("PREFS", 0);
                         SharedPreferences.Editor editor = settings.edit();
                         editor.putString("password", newPassword);
                         editor.apply();
 
-                        Toast.makeText(CreatePasswordActivity.this,
+                        Toast.makeText(ChangePasswordActivity.this,
                                 "Password Saved Successfully",
                                 Toast.LENGTH_SHORT)
                                 .show();
@@ -58,9 +69,10 @@ public class CreatePasswordActivity extends AppCompatActivity {
                         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(intent);
                         //finish();
+                    }
 
-                    } else {
-                        //there is no match for the passwords
+                    //there is no match for the passwords
+                    if (!newPassword.equals(repeatPassword)) {
                         editRepeatPassword.setError("Passwords Don't Match");
                     }
                 }
@@ -75,5 +87,12 @@ public class CreatePasswordActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public String getCurrentPassword() {
+        SharedPreferences settings = getSharedPreferences(
+                "PREFS", 0);
+        String currentPassword = settings.getString("password", null);
+        return currentPassword;
     }
 }
--- app/src/main/java/com/example/readyourresults/Settings/SettingsFragment.java
+++ app/src/main/java/com/example/readyourresults/Settings/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.readyourresults.Settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -7,7 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.readyourresults.Password.ChangePasswordActivity;
+import com.example.readyourresults.Password.CreatePasswordActivity;
 import com.example.readyourresults.R;
 
 public class SettingsFragment extends Fragment {
@@ -15,5 +19,18 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Button changePasswordButton = getView().findViewById(R.id.button);
+        changePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), ChangePasswordActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
     }
 }
