8803611d8cedc74f991d0ba2951c8f8d468925a7
vab10266 <vaudalexburton@gmail.com>
2019-07-12 11:52:30 -0400

    cleaned up ImageProcessor

--- app/src/main/java/com/example/readyourresults/Preprocessing/ImageProcessor.java
+++ app/src/main/java/com/example/readyourresults/Preprocessing/ImageProcessor.java
@@ -69,32 +69,12 @@ public class ImageProcessor {
     private Bitmap btm;
     String path;
     public ImageProcessor(File img) {
-        //OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_11, this, baseLoaderCallback );
 
         Mat src = new Mat();
         src = imread(img.getAbsolutePath());
         Mat out = new Mat();
         out = imread(img.getAbsolutePath());
 
-//        Point a,b;
-//        a = new Point(src.rows()/6,src.cols()/4);
-//        b = new Point(src.rows()*2/3, src.cols()*13/16);
-//        Rect rectCropa = new Rect((int)a.x, (int)a.y , (int)(b.x-a.x+1), (int)(b.y-a.y+1));
-//        src = src.submat(rectCropa);
-
-
-        //70x125
-        //70x95
-        //bitmapToMat(m,m,false);
-//        /*
-//        for (int i = 10; i <= 50; i+=10) {
-//            for (int j = 15; j <= 55; j+=10) {
-//                Canny(src, out, i, j, false);
-//                path = img.getAbsolutePath().substring(0, img.getAbsolutePath().lastIndexOf(".jpg")) + ":" + i + "x" + j + "Gray_Image.bmp";
-//                imwrite(path, out);
-//            }
-//        }
-//        */
         path = img.getAbsolutePath().substring(0, img.getAbsolutePath().lastIndexOf(".jpg")) + ":" + "corn.bmp";
         Point[] corns = detectSquare(src,path);
         Boolean cropped = false;
@@ -136,76 +116,6 @@ public class ImageProcessor {
         path = img.getAbsolutePath().substring(0, img.getAbsolutePath().lastIndexOf(".")) + ":equal" + "new.bmp";
         imwrite(path, retVal);
 
-//        Log.d(TAG,"type1:" + out.type());
-//        Log.d(TAG,"type2:" + edge.type());
-//        //equalizeHist(edge,edge);
-//        path = img.getAbsolutePath().substring(0, img.getAbsolutePath().lastIndexOf(".jpg")) + ":" + ":helpme.bmp";
-//        imwrite(path, edge);
-//        Mat circles = new Mat();
-//////        Imgproc.HoughCircles(edge, circles, Imgproc.HOUGH_GRADIENT, 1.0,
-//////                (double) gray.rows() / 16, // change this value to detect circles with different distances to each other
-//////                20.0, 25.0, gray.cols()/6, gray.cols()/3);
-////        for (int i = 0; i <= 60; i+=10) {
-////            for (int j = 5; j <= 65; j += 10) {
-//////                Canny(edge, circles, i, j, false);
-//////                path = img.getAbsolutePath().substring(0, img.getAbsolutePath().lastIndexOf(".jpg")) + ":"+i+"x"+j + ":edges.bmp";
-//////                imwrite(path, circles);
-////            }
-////        }
-////
-//////        //Imgproc.medianBlur(gray, gray, 5);
-//////        /*
-//////        Mat circles = new Mat();
-//////        //for (int i = 0; circles.get(0,0) == null && i < 60; i += 10) {
-//////            Log.d(TAG,"startCirc");
-//////            Imgproc.HoughCircles(gray, circles, Imgproc.HOUGH_GRADIENT, 1.0,
-//////                    (double) gray.rows() / 16, // change this value to detect circles with different distances to each other
-//////                    20.0, 25.0, gray.cols()/6, gray.cols()/3); // change the last two parameters
-//////            Log.d(TAG,"end");
-//////        //}
-//////
-//////        // (min_radius & max_radius) to detect larger circles
-//////        for (int x = 0; x < circles.cols(); x++) {
-//////            double[] c = circles.get(0, x);
-//////            Point center = new Point(Math.round(c[0]), Math.round(c[1]));
-//////            // circle center
-//////            Imgproc.circle(src, center, 1, new Scalar(0,100,100), 2, 8, 0 );
-//////            // circle outline
-//////            int radius = (int) Math.round(c[2]);
-//////            Imgproc.circle(src, center, radius, new Scalar(255,0,255), 2, 8, 0 );
-//////        }
-//////        path = img.getAbsolutePath().substring(0, img.getAbsolutePath().lastIndexOf(".jpg")) + ":" + "circs.bmp";
-//////        imwrite(path, src);
-//////
-//////        //Informed crop
-//////        Point p1,p4;
-//////        double[] circle = circles.get(0,0);
-//////        Point center = new Point(Math.round(circle[0]), Math.round(circle[1]));
-//////        int rad = (int) Math.round(circle[2]);
-//////        p1 = new Point(center.x-rad,center.y-rad);
-//////        p4 = new Point(center.x+rad,center.y+rad);
-//////        Rect rectCrop = new Rect((int)p1.x, (int)p1.y , (int)(p4.x-p1.x+1), (int)(p4.y-p1.y+1));
-//////        src = src.submat(rectCrop);
-//////        */
-//        Imgproc.cvtColor(out, gray, Imgproc.COLOR_BGR2GRAY);
-//        equalizeHist(gray,out);
-//        //Mat comb = gray;
-//        //add(gray,src,comb);
-//
-//        //Canny(src, src, 100, 60, false);
-//
-//        path = img.getAbsolutePath().substring(0, img.getAbsolutePath().lastIndexOf(".jpg")) + ":" + "Circle.bmp";
-//        imwrite(path, out);
-//        Log.d(TAG,path);
-////        int j = 260;
-////        for (int i = j; i <= j+70; i += 10) {
-////            Canny(src, src, i, i+40, false);
-////            path = img.getAbsolutePath().substring(0, img.getAbsolutePath().lastIndexOf(".jpg")) + ":" +i+ "edge.bmp";
-////            imwrite(path, src);
-////        }
-//        btm = BitmapFactory.decodeFile(img.getAbsolutePath());
-//        src = imread(img.getAbsolutePath());
-//        //Utils.matToBitmap(src, btm);
     }
     public String toString() {
         return path;
@@ -259,82 +169,6 @@ public class ImageProcessor {
         Rect crop = new Rect(rect[0], rect[1]);
 
 
-//        imwrite(path, resized);
-//        path = path + "2.bmp";
-//        List<MatOfPoint> conts = new ArrayList<>();
-//        Mat hierarchy = new Mat();
-//        findContours(gray,conts,hierarchy,RETR_EXTERNAL,CHAIN_APPROX_SIMPLE);
-//        Log.d(TAG,"Start Looking");
-//        Mat circles = new Mat();
-//        Imgproc.HoughCircles(gray, circles, Imgproc.HOUGH_GRADIENT, 1.0, 1, // change this value to detect circles with different distances to each other
-//                10, 50, 30, 100); // change the last two parameters
-//        Log.d(TAG,"end");
-//
-//
-//         //(min_radius & max_radius) to detect larger circles
-//        for (int x = 0; x < circles.cols(); x++) {
-//            double[] c = circles.get(0, x);
-//            Point center = new Point(Math.round(c[0]), Math.round(c[1]));
-//            // circle center
-//            Imgproc.circle(resized, center, 1, new Scalar(0,100,100), 3, 8, 0 );
-//            // circle outline
-//            int radius = (int) Math.round(c[2]);
-//            Imgproc.circle(resized, center, radius, new Scalar(255,0,255), 3, 8, 0 );
-//        }
-//        for (MatOfPoint cur : conts) {
-//            MatOfPoint2f curve = new MatOfPoint2f(cur.toArray());
-//            double perimiter = arcLength(curve, true);
-//            //Log.d(TAG,perimiter + "");
-//            approxPolyDP(curve, curve, 0.04 * perimiter, true);
-//            MatOfPoint approx = new MatOfPoint(curve.toArray());
-//
-//            if (perimiter >= 200/*curve.toArray().length == 4*/) {
-//                Rect bounds = boundingRect(approx);
-//                float ar = 1.0f * bounds.width / bounds.height;
-//                if (ar < 1.8f && ar > .55f) {
-//                    Point cent = new Point(bounds.x + bounds.width / 2, bounds.y + bounds.height / 2);
-//                    if (bounds.width < resized.width()*6/8 && cent.x > resized.width()/4 && cent.x < resized.width()*3/4 && cent.y > resized.height()/4 && cent.y < resized.height()*3/4) {
-//                        points.add(new Point(bounds.x, bounds.y));
-//                        points.add(new Point(bounds.x + bounds.width, bounds.y + bounds.height));
-//                        Imgproc.circle(resized, cent, bounds.width / 2, new Scalar(255, 0, 255), 3, 8, 0);
-//                        Imgproc.circle(resized, cent, bounds.height / 2, new Scalar(255, 0, 255), 3, 8, 0);
-//
-//                    }
-//                }
-//            }
-//        }
-//        double avgtlx = 0;
-//        double avgtly = 0;
-//        double avgbrx = 0;
-//        double avgbry = 0;
-//        int count = 0;
-//        while (points.size() > 1) {
-//            Point tl = points.remove(0);
-//            Point br = points.remove(0);
-//            avgtlx += tl.x;
-//            avgtly += tl.y;
-//            avgbrx += br.x;
-//            avgbry += br.y;
-//            count++;
-//        }
-//        avgtlx /= count;
-//        avgtly /= count;
-//        avgbrx /= count;
-//        avgbry /= count;
-//        avgtlx /= ratio;
-//        avgtly /= ratio;
-//        avgbrx /= ratio;
-//        avgbry /= ratio;
-//        Point tl = new Point(avgtlx, avgtly);
-//        Point br = new Point(avgbrx, avgbry);
-//        Log.d(TAG,ratio + "ra");
-//        Log.d(TAG,resized.width() + "re");
-//        Log.d(TAG,img.width() + "im");
-//        Imgproc.circle(resized, tl, 2, new Scalar(255, 0, 255), 3, 8, 0);
-//        Imgproc.circle(resized, br, 2, new Scalar(255, 0, 255), 3, 8, 0);
-//        path = path + "1.bmp";
-//        imwrite(path, resized);
-//        Point[] retVal = {tl,br};
         return rect;
     }
 
@@ -371,8 +205,6 @@ public class ImageProcessor {
                         Log.d(TAG, i + ", "+x1+", oth, " + slope);
                     }
                 }
-//                if (verts.contains(re) || horis.contains(re))
-//                    line(resized,new Point(x1,y1),new Point(x2,y2), new Scalar(255, 0, 0));
             }
         }
         Log.d(TAG, "Merging");
@@ -417,21 +249,17 @@ public class ImageProcessor {
 
             if (x2 == x1) {
                 //vertical
-//                Log.d(TAG, ", "+x1+", v, perfect");
                 verts.add(line);
             } else {
                 double slope = (y2 - y1) / (x2 / x1);
                 if (slope > 15 || slope < -15) {
                     //vertical
                     verts.add(line);
-//                    Log.d(TAG, ", "+x1+", v, " + slope);
                 } else if (slope < 1.0 / 15 && slope > -1.0 / 15) {
                     //horizontal
                     horis.add(line);
-//                    Log.d(TAG,  ", "+x1+", h, " + slope);
                 } else {
                     //other
-//                    Log.d(TAG, ", "+x1+", oth, " + slope);
                 }
             }
 
